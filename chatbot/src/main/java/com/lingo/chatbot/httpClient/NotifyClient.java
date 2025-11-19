df2bac079ff9cdaad10ad6a2dc84d49976631887
Vin Com <0plmzxc@gmail.com>
2025-11-19 21:40:17 +0700

    solve bean notify not run

--- chatbot/src/main/java/com/lingo/chatbot/httpClient/NotifyClient.java
+++ chatbot/src/main/java/com/lingo/chatbot/httpClient/NotifyClient.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
